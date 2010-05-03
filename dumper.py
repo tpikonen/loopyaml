d7f0557c3e75cb45ae9380388e5c9445fa7a2d40
Teemu Ikonen <tpikonen@gmail.com>
2010-05-03 17:52:59 +0200

    representer.py: New file
    Move representer functions here.
    Add Looprepresenter class which prints floats with exponential format with
    a size given in 'float_width' parameter.

--- dumper.py
+++ dumper.py
@@ -3,76 +3,11 @@ from yaml.representer import *
 from yaml.resolver import *
 from emitter import *
 from serializer import *
+from representer import *
 from loopdict import *
 
 
-def represent_loopsequence(self, tag, sequence, columns, flow_style=True):
-    value = []
-    node = LoopSequenceNode(tag, value, columns, flow_style=flow_style)
-    if self.alias_key is not None:
-        self.represented_objects[self.alias_key] = node
-    best_style = True
-    for item in sequence:
-        node_item = self.represent_data(item)
-        if not (isinstance(node_item, ScalarNode) and not node_item.style):
-            best_style = False
-        value.append(node_item)
-    if flow_style is None:
-        if self.default_flow_style is not None:
-            node.flow_style = self.default_flow_style
-        else:
-            node.flow_style = best_style
-    return node
-
-
-def loopdict_representer(self, data, flow_style=False):
-    """PyYAML representer for the Loopdict type."""
-    # self is the yaml.Representer object
-    dnorm = data.copy()
-    dloop = {}
-    for k in data.loopvars:
-        dloop[k] = dnorm.pop(k)
-    tag = u'tag:yaml.org,2002:map'
-    mapping = dnorm
-    # copypaste from represent_mapping
-    value = []
-    node = MappingNode(tag, value, flow_style=flow_style)
-    if self.alias_key is not None:
-        self.represented_objects[self.alias_key] = node
-    best_style = True
-    if hasattr(mapping, 'items'):
-        mapping = mapping.items()
-        mapping.sort()
-    for item_key, item_value in mapping:
-        node_key = self.represent_data(item_key)
-        node_value = self.represent_data(item_value)
-        if not (isinstance(node_key, ScalarNode) and not node_key.style):
-            best_style = False
-        if not (isinstance(node_value, ScalarNode) and not node_value.style):
-            best_style = False
-        value.append((node_key, node_value))
-    if flow_style is None:
-        if self.default_flow_style is not None:
-            node.flow_style = self.default_flow_style
-        else:
-            node.flow_style = best_style
-    # copypaste ends
-    vars_key = self.represent_data('=cols')
-    vars_item = self.represent_data(data.loopvars)
-    value.append((vars_key, vars_item))
-    loopseq = []
-    for i in range(len(data[data.loopvars[0]])):
-        for k in data.loopvars:
-            loopseq.append(data[k][i])
-    loop_key = self.represent_data('=loop')
-    loop_item = represent_loopsequence(self, u'tag:yaml.org,2002:seq', loopseq,
-            len(data.loopvars))
-    value.append((loop_key, loop_item))
-
-    return node
-
-
-class LoopDumper(LoopEmitter, LoopSerializer, Representer, Resolver):
+class LoopDumper(LoopEmitter, LoopSerializer, LoopRepresenter, Resolver):
 
     def __init__(self, stream,
             default_style=None, default_flow_style=None,
@@ -86,7 +21,7 @@ class LoopDumper(LoopEmitter, LoopSerializer, Representer, Resolver):
         LoopSerializer.__init__(self, encoding=encoding,
                 explicit_start=explicit_start, explicit_end=explicit_end,
                 version=version, tags=tags)
-        Representer.__init__(self, default_style=default_style,
+        LoopRepresenter.__init__(self, default_style=default_style,
                 default_flow_style=default_flow_style)
         Resolver.__init__(self)
         self.add_representer(Loopdict, loopdict_representer)
---
+++ representer.py
@@ -0,0 +1,100 @@
+from yaml.representer import *
+from yaml.nodes import *
+from loopsequence import *
+
+def represent_loopsequence(self, tag, sequence, columns, flow_style=True):
+    value = []
+    node = LoopSequenceNode(tag, value, columns, flow_style=flow_style)
+    if self.alias_key is not None:
+        self.represented_objects[self.alias_key] = node
+    best_style = True
+    for item in sequence:
+        node_item = self.represent_data(item)
+        if not (isinstance(node_item, ScalarNode) and not node_item.style):
+            best_style = False
+        value.append(node_item)
+    if flow_style is None:
+        if self.default_flow_style is not None:
+            node.flow_style = self.default_flow_style
+        else:
+            node.flow_style = best_style
+    return node
+
+
+def loopdict_representer(self, data, flow_style=False):
+    """PyYAML representer for the Loopdict type."""
+    # self is the yaml.Representer object
+    dnorm = data.copy()
+    dloop = {}
+    for k in data.loopvars:
+        dloop[k] = dnorm.pop(k)
+    tag = u'tag:yaml.org,2002:map'
+    mapping = dnorm
+    # copypaste from represent_mapping
+    value = []
+    node = MappingNode(tag, value, flow_style=flow_style)
+    if self.alias_key is not None:
+        self.represented_objects[self.alias_key] = node
+    best_style = True
+    if hasattr(mapping, 'items'):
+        mapping = mapping.items()
+        mapping.sort()
+    for item_key, item_value in mapping:
+        node_key = self.represent_data(item_key)
+        node_value = self.represent_data(item_value)
+        if not (isinstance(node_key, ScalarNode) and not node_key.style):
+            best_style = False
+        if not (isinstance(node_value, ScalarNode) and not node_value.style):
+            best_style = False
+        value.append((node_key, node_value))
+    if flow_style is None:
+        if self.default_flow_style is not None:
+            node.flow_style = self.default_flow_style
+        else:
+            node.flow_style = best_style
+    # copypaste ends
+    vars_key = self.represent_data('=cols')
+    vars_item = self.represent_data(data.loopvars)
+    value.append((vars_key, vars_item))
+    loopseq = []
+    for i in range(len(data[data.loopvars[0]])):
+        for k in data.loopvars:
+            loopseq.append(data[k][i])
+    loop_key = self.represent_data('=loop')
+    loop_item = represent_loopsequence(self, u'tag:yaml.org,2002:seq', loopseq,
+            len(data.loopvars))
+    value.append((loop_key, loop_item))
+
+    return node
+
+
+class LoopRepresenter(Representer):
+    def __init__(self, float_width=15, **kwargs):
+        if float_width < 7:
+            raise ValueError("Minimum width of the float field must be >= 7")
+        self.float_width = float_width
+        Representer.__init__(self, **kwargs)
+
+    def represent_float(self, data):
+        if data != data or (data == 0.0 and data == 1.0):
+            value = u'.nan'
+        elif data == self.inf_value:
+            value = u'.inf'
+        elif data == -self.inf_value:
+            value = u'-.inf'
+        else:
+            #value = unicode(repr(data)).lower()
+            value = u'%#0*.*e' % (self.float_width, self.float_width-5, data)
+            # Note that in some cases `repr(data)` represents a float number
+            # without the decimal parts.  For instance:
+            #   >>> repr(1e17)
+            #   '1e17'
+            # Unfortunately, this is not a valid float representation according
+            # to the definition of the `!!float` tag.  We fix this by adding
+            # '.0' before the 'e' symbol.
+            if u'.' not in value and u'e' in value:
+                value = value.replace(u'e', u'.0e', 1)
+        return self.represent_scalar(u'tag:yaml.org,2002:float', value)
+
+LoopRepresenter.add_representer(float,
+        LoopRepresenter.represent_float)
